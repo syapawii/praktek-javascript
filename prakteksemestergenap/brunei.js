6bbc7d37d97cafb0139446e644195e60d44fc215
syapawii <22_malwiridho@student.smkti.net>
2024-01-15 11:00:01 +0700

    first commit

---
+++ prakteksemestergenap/brunei.js
@@ -0,0 +1,13 @@
+const negara = "brunei darussalam";
+const benua = "asia";
+
+let populasi = 445.373;
+
+let kepulauan = true;
+var bahasa = "bahasa melayu";
+
+console.log("negara: " + negara);
+console.log("benua: " + benua);
+console.log("populasi: " + populasi);
+console.log("kepulauan: " + kepulauan);
+console.log("bahasa: " + bahasa);
