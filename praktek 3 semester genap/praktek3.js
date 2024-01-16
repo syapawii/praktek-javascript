d9635ce4d52698c3135cc3c3a4b67bbc6a1fe944
syapawii <22_malwiridho@student.smkti.net>
2024-01-16 14:27:15 +0700

    praktik ketiga

---
+++ praktek 3 semester genap/praktek3.js
@@ -0,0 +1,15 @@
+const negara = "brunei darussalam";
+const benua = "asia";
+
+let populasi = 445.373;
+
+let kepulauan = true;
+var bahasa = "bahasa melayu";
+
+console.log("negara: " + negara);
+console.log("benua: " + benua);
+console.log("populasi: " + populasi);
+console.log("kepulauan: " + kepulauan);
+console.log("bahasa: " + bahasa);
+
+console.log(`negara ${negara} adalah negara yang berada di benua ${benua} dengan populasi ${populasi} miliar jiwa, bahasa nasional ${bahasa}`);
